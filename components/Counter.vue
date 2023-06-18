04e7fb91e7833fc744d36d092cc7952257e29cb1
yoneda <nsshowdream@gmail.com>
2023-06-18 21:25:21 +0900

    イベントの購読を作成

--- components/Counter.vue
+++ components/Counter.vue
@@ -1,35 +1,39 @@
 <template>
     <p class="test">this is sample text</p>
-    <p>this is sample text too {{ testValue }}</p>
-    <p>this is counter {{ count }}</p>
+    <p>this is sample text too :{{ testValue }}</p>
+    <p>this is counter :{{ count }}</p>
+    <p>this is component name :{{ name }}</p>
     <button @click="testFunc">click me</button>
+    <button @click="$emit('updateGlobalCount')">update global count</button>
 </template>
 
-<script lang="ts">
+<script>
 // 通常のVue3-optionsAPIでのコンポーネントの書き方
-// export default {
-//     data: () => ({
-//         count: 0,
-//         testValue: "hey, tarou"
-//     }),
-//     methods:{
-//         testFunc(){
-//             this.count++
-//         }
-//     }
-// }
-
-// Vue3-optionsAPIでのvue-facing-decoratorを使ったクラス記法っぽい書き方
-import { Component, Vue } from 'vue-facing-decorator'
-@Component
-export default class Counter extends Vue{
-    count = 0
-    testValue = "hey, smith"
-
-    testFunc(){
-        this.count++
+export default {
+    props: ['name'],
+    emits: ['updateGlobalCount'],
+    data: () => ({
+        count: 0,
+        testValue: "hey, tarou"
+    }),
+    methods:{
+        testFunc(){
+            this.count++
+        }
     }
 }
+
+// Vue3-optionsAPIでのvue-facing-decoratorを使ったクラス記法っぽい書き方
+// import { Component, Vue } from 'vue-facing-decorator'
+// @Component
+// export default class Counter extends Vue{
+//     count = 0
+//     testValue = "hey, smith"
+
+//     testFunc(){
+//         this.count++
+//     }
+// }
 
 
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,25 @@
       <AppAlert>
         This is an auto-imported component
       </AppAlert>
-      <Counter />
+      <div>this is global count: {{ globalCount }}</div>
+      <Counter name="counter component"  @update-global-count="increment"/>
     </div>
 </template>
+
+<script>
+import Counter from '../components/Counter.vue'
+
+export default {
+  components: {
+    Counter
+  },
+  data: () => ({
+      globalCount:0
+  }),
+  methods:{
+    increment(){
+      this.globalCount++
+    }
+  }
+}
+</script>
