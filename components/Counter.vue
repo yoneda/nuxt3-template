05e09adf0f34c52ea391f5608efaeae57cb1ad17
yoneda <nsshowdream@gmail.com>
2023-06-18 19:02:07 +0900

    vue-facing-decoratorを使用してコンポーネントを実装

--- components/Counter.vue
+++ components/Counter.vue
@@ -5,18 +5,33 @@
     <button @click="testFunc">click me</button>
 </template>
 
-<script>
-export default {
-    data: () => ({
-        count: 0,
-        testValue: "hey, tarou"
-    }),
-    methods:{
-        testFunc(){
-            this.count++
-        }
+<script lang="ts">
+// 通常のVue3-optionsAPIでのコンポーネントの書き方
+// export default {
+//     data: () => ({
+//         count: 0,
+//         testValue: "hey, tarou"
+//     }),
+//     methods:{
+//         testFunc(){
+//             this.count++
+//         }
+//     }
+// }
+
+// Vue3-optionsAPIでのvue-facing-decoratorを使ったクラス記法っぽい書き方
+import { Component, Vue } from 'vue-facing-decorator'
+@Component
+export default class Counter extends Vue{
+    count = 0
+    testValue = "hey, smith"
+
+    testFunc(){
+        this.count++
     }
 }
+
+
 </script>
 
 <style>
